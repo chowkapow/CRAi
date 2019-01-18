33689465a7053c022484077c27c711033db85457
Eric Chow <eric.zjp.chow@gmail.com>
2019-01-18 13:51:01 -0600

    Rename routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,20 +7,19 @@ let actor;
 
 router.post('/search-actor', (req, res) => {
   actor = req.body.actor.trim();
-
   if (!getCast().has(actor)) res.redirect('/error');
   else res.sendStatus(200);
 });
 
-router.get('/search-actor-filmography', async (req, res) => {
+router.get('/actor-filmography', async (req, res) => {
   const filmography = await getActorFilmography(actor);
-  if (filmography) res.send({ filmography });
+  if (filmography) res.send(filmography);
   else res.redirect('/error');
 });
 
-router.get('/search-actor-picture', async (req, res) => {
+router.get('/actor-picture', async (req, res) => {
   const picture = await getActorPicture(actor);
-  if (picture) res.send({ picture });
+  if (picture) res.send(picture);
 });
 
 module.exports = router;
