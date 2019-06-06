d5e4c0b5ae4f1750990c53edb73387dbf26db8ab
Eric Chow <e.chow@accenture.com>
2019-06-06 15:49:04 -0500

    Remove global variable

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,18 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { getActorFilmography, getCast, getActorPicture } = require('../utils');
-
-let actor;
-
-router.post('/search-actor', (req, res) => {
-  actor = req.body.actor.trim();
-  if (!getCast().has(actor)) res.redirect('/error');
-  else res.sendStatus(200);
-});
+const { getActorFilmography, getActorPicture } = require('../utils');
 
 router.get('/actor-filmography', async (req, res) => {
-  const filmography = await getActorFilmography(actor);
+  const filmography = await getActorFilmography(req.query.actor);
   if (filmography) res.send(filmography);
   else res.redirect('/error');
 });
