5b099e8584405cf384cc57b85065696906a88e7b
Eric Chow <eric.zjp.chow@gmail.com>
2019-12-10 23:38:43 -0600

    Prepare for Heroku deployment
    Add Procfile, root/package.json
    Change scripts to use babel
    Tell Express to use React for static files
    Add catch-all handler

--- server/src/app.js
+++ server/src/app.js
@@ -10,17 +10,21 @@ import actorRouter from './routes/actor';
 import { setCast } from './routes/utils';
 
 const app = express();
+const buildPath = path.join(__dirname, '../../client/build');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(buildPath));
 
 setCast();
 
-app.use('/', indexRouter);
 app.use('/actor', actorRouter);
+app.use('/', indexRouter);
+app.get('*', (req, res) => {
+  res.sendFile('index.html', { root: buildPath });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -33,9 +37,8 @@ app.use(function(err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({ message: err.message, error: err });
 });
 
 export default app;
